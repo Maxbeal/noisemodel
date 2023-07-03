09b8c2a882c232b881300ca792c5c13e17629335
romainsacchi <r_s@me.com>
2023-07-03 14:39:53 +0200

    Library skeleton

---
+++ noisemodel/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+__version__ = (0, 0, 1)
+__all__ = ("__version__", "DATA_DIR", "NoiseEmissions")
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+
+from .noise import NoiseEmissions
---
+++ noisemodel/noise.py
@@ -0,0 +1,8 @@
+import py_wake
+class NoiseEmissions():
+
+    def __init__(self, name):
+        self.name = name
+
+    def whatsmyname(self):
+        print("My name is " + self.name)
